01a4a7ee4dd1d963200df62524c1f2030feefec4
paulop2 <paulo225vitor@gmail.com>
2020-06-13 15:53:49 -0300

    Arquivos da Aula 5

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -66,6 +66,8 @@ function handleSelectedItem (event) {
 
     const itemId = itemLi.dataset.id
     
+    // console.log('ITEM ID: ', itemId)
+
     //verificar se existem itens selecionados.
     //Se sim, pegar os itens selecionados
     
@@ -90,6 +92,8 @@ function handleSelectedItem (event) {
         selectedItems.push(itemId)
 
     }
+
+    // console.log('selectedItems: ',selectedItems)
 
     //atualizar o campo escondido com os dados selecionados
     collectedItems.value = selectedItems
